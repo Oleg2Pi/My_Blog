0e5c7cf45bb213651e17c513f6198175a1990f04
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-01-16 19:18:14 +0300

    Change function post_list to class PostListView and change urls with list.html (page_obj instead of posts

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,27 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # from django.http import Http404
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    # Постраничная разбивка с 3 постами на страницу
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # Если page_number не целое число, то выдать певрую страницу
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Если page_number находится в не диапазона, то выдать последнюю страницу
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts},
-                  )
-
+# def post_list(request):
+#     post_list = Post.published.all()
+#     # Постраничная разбивка с 3 постами на страницу
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # Если page_number не целое число, то выдать певрую страницу
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # Если page_number находится в не диапазона, то выдать последнюю страницу
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request,
+#                   'blog/post/list.html',
+#                   {'posts': posts},
+#                   )
+# Вместо представления списка постов функцией
+class PostListView(ListView):
+    # Типовой набор запросов (вместо queryset) если указать model=Post - это = queryset=Post.objects.all()
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 def post_detail(request, year, month, day, post):
     # try:
